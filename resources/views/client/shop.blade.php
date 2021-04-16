cf83214b1aef4dd7e1853acdbf14d7c6d2048980
Ping <panfilo.glophics@gmail.com>
2021-04-17 01:46:37 +0800

    notifications

--- resources/views/client/shop.blade.php
+++ resources/views/client/shop.blade.php
@@ -105,6 +105,15 @@
             }
         });
 
+        $("#quantity").on("input", function(evt) {
+            var self = $(this);
+            self.val(self.val().replace(/[^0-9\.]/g, ''));
+            if ((evt.which != 46 || self.val().indexOf('.') != -1) && (evt.which < 48 || evt.which > 57)) 
+            {
+                evt.preventDefault();
+            }
+        });
+
         //declare local variables
         var prod_id = '';
         var prod_image = '';
